acd5b66e3292413df82fc6cd1d988f86c711d4ee
unknown <jeremyshawnjackson@gmail.com>
2021-10-09 19:25:48 -0700

    made changes to genre mappings. added source dataset

--- summaries_to_json.py
+++ summaries_to_json.py
@@ -3,6 +3,7 @@ import json
 with open('booksummaries.txt', 'r', encoding="utf8") as summaries, open('summaries.json', 'w', encoding='utf-8') as jsonfile:
   books=[]
   fields = ['title', 'genres', 'book_desc']
+  fiction = {'Fantasy', 'Children\'s literature', 'Mystery', 'Young adult literature', 'Suspense', 'Romance novel', 'Adventure novel', 'Alternate history', 'Satire', 'Steampunk'}
   # valid_genres = {'Fiction', 'Science Fiction', 'Fantasy', 'Children\'s literature', 'Mystery', 'Suspense', 'Thriller', 'Horror', 'Romance novel', 'Non-fiction'}
   
   for line in summaries:
@@ -21,6 +22,11 @@ with open('booksummaries.txt', 'r', encoding="utf8") as summaries, open('summari
 
     if 'Non-fiction' in book['genres'] and 'Fiction' in book['genres']:
       book['genres'].pop(book['genres'].index('Fiction'))
+    
+    # ensure fictional genres are categorized as such
+    if bool(set(book['genres']) & fiction):
+      book['genres'].append('Fiction')
+    
     for key in book['genres']:
       if 'Fiction' in key or ' fiction' in key:
         book['genres'].append('Fiction')
